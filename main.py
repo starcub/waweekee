648da1f03e3e45866a5aed29813676a97ba11d3d
Shawn Jiang <xuancn@gmail.com>
2010-04-20 23:18:43 +1200

    Added N-gram, and strip punctuations and stop words

--- main.py
+++ main.py
@@ -18,6 +18,7 @@
 import cgi
 import urllib2
 import os
+import re, string
 
 from google.appengine.api import users
 from google.appengine.ext import webapp
@@ -76,10 +77,83 @@ class Wikify(webapp.RequestHandler):
     
   def wikifying(self,text):
     wikifiedText = text.split()
-
+	
+class Ngram(webapp.RequestHandler):
+    def post(self):
+        text = cgi.escape(self.request.get('content1'))
+        gram1 = text.split()
+        gram2=[]
+        gram3=[]
+        gram4=[]
+        for i in range(0,len(gram1)-1):
+            gram2.append(gram1[i] + ' ' + gram1[i+1])
+        for i in range(0,len(gram1)-2):
+            gram3.append(gram1[i] + ' ' + gram1[i+1] + ' ' + gram1[i+2])
+        for i in range(0,len(gram1)-3):
+            gram4.append(gram1[i] + ' ' + gram1[i+1] + ' ' + gram1[i+2] + ' ' + gram1[i+3])
+        self.response.out.write('<html><body>')
+        self.response.out.write('<h1>Unstripped</h1>')
+        self.response.out.write('unigram:<pre>')
+        self.response.out.write(gram1)
+        self.response.out.write('</pre>')
+        self.response.out.write('bigram:<pre>')
+        self.response.out.write(gram2)
+        self.response.out.write('</pre>')
+        self.response.out.write('trigram:<pre>')
+        self.response.out.write(gram3)
+        self.response.out.write('</pre>')
+        self.response.out.write('quadgram:<pre>')
+        self.response.out.write(gram4)
+        self.response.out.write('</pre>')
+        self.response.out.write('<h1>Stripped Punctuations and Stop words</h1>')
+        self.response.out.write('unigram:<pre>')
+        self.response.out.write(self.stripStopwords(self.stripPunctuation(gram1)))
+        self.response.out.write('</pre>')
+        self.response.out.write('bigram:<pre>')
+        self.response.out.write(self.stripStopwords(self.stripPunctuation(gram2)))
+        self.response.out.write('</pre>')
+        self.response.out.write('trigram:<pre>')
+        self.response.out.write(self.stripStopwords(self.stripPunctuation(gram3)))
+        self.response.out.write('</pre>')
+        self.response.out.write('quadgram:<pre>')
+        self.response.out.write(self.stripStopwords(self.stripPunctuation(gram4)))
+        self.response.out.write('</pre>')
+        self.response.out.write('</pre></body></html>')
+        
+    def stripPunctuation(self, word_list):
+        punctuation = re.compile(r'[.?!,":;]')
+        punctuation = re.compile('[%s]' % re.escape(string.punctuation))
+        #
+        wl2 = []
+        for word in word_list:
+        #
+        # remove punctuation marks
+        #
+            word2 = punctuation.sub("", word)
+            wl2.append(word2)
+        return wl2
+    def stripStopwords(self, word_list):
+        stopwords = ["a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+        index=0
+        while index < len(word_list):
+            word = word_list[index]
+            if len(word.split()) < 2:
+                if word.split()[0].lower() in stopwords:
+                    word_list.remove(word_list[index])
+                else:
+                    index= index+1
+                continue
+            else:
+                if word.split()[0].lower() in stopwords or word.split()[len(word.split())-1].lower() in stopwords:
+                    word_list.remove(word_list[index])
+                else:
+                    index= index+1
+        return word_list
+        
 application = webapp.WSGIApplication(
                                      [('/', MainPage),
-                                      ('/wikify', Wikify)],
+                                      ('/wikify', Wikify),
+									  ('/ngram', Ngram)],
                                      debug=True)
 
 def main():
